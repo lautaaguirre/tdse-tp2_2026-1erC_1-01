eb7460743c013bf99ea5bd177e15af1c09228df2
Lautaro Aguirre <lfaguirre@fi.uba.ar>
2026-05-05 18:12:53 -0300

    ACTIVIDAD 2: Configuracion de 3 sensores

---
+++ tdse-tp2_02-model_integration/app/inc/task_sensor_attribute.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2026 Juan Manuel Cruz <jcruz@fi.uba.ar> <jcruz@frba.utn.edu.ar>.
+ * All rights reserved.
+ * ... (Licencia omitida por brevedad, mantener en archivo final)
+ */
+
+#ifndef TASK_SENSOR_ATTRIBUTE_H_
+#define TASK_SENSOR_ATTRIBUTE_H_
+
+/********************** CPP guard ********************************************/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/********************** inclusions *******************************************/
+#include "task_system_attribute.h"
+/********************** macros ***********************************************/
+
+/********************** typedef **********************************************/
+/* Events to excite Task Sensor */
+typedef enum task_sensor_ev {
+    EV_BTN_UP,
+    EV_BTN_DOWN
+} task_sensor_ev_t;
+
+/* States of Task Sensor */
+typedef enum task_sensor_st {
+    ST_BTN_UP,
+    ST_BTN_FALLING,
+    ST_BTN_DOWN,
+    ST_BTN_RISING
+} task_sensor_st_t;
+
+/* Identifier of Task Sensor (Solo 1 para la Actividad 01) */
+/* Identifier of Task Sensor */
+typedef enum task_sensor_id {
+    ID_BTN_A,
+	ID_BTN_B, // Sustituimos el A, o lo agregamos. Usaremos B, C y D para esta actividad.
+    ID_BTN_C,
+    ID_BTN_D
+} task_sensor_id_t;
+
+typedef struct
+{
+	task_sensor_id_t	identifier;
+	GPIO_TypeDef *		gpio_port;
+	uint16_t			pin;
+	GPIO_PinState		pressed;
+	uint32_t			tick_max;
+	task_system_ev_t	signal_up;
+	task_system_ev_t	signal_down;
+} task_sensor_cfg_t;
+
+typedef struct
+{
+	uint32_t			tick;
+	task_sensor_st_t	state;
+	task_sensor_ev_t	event;
+} task_sensor_dta_t;
+
+/********************** external data declaration ****************************/
+extern task_sensor_dta_t task_sensor_dta_list[];
+
+/********************** external functions declaration ***********************/
+
+/********************** End of CPP guard *************************************/
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TASK_SENSOR_ATTRIBUTE_H_ */
+
+/********************** end of file ******************************************/
